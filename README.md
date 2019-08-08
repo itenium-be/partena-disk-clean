e286af32c71ed480bbd3adeb4875ff3836bad083
Wouter Van Schandevijl <woutervs@hotmail.com>
2019-08-08 11:46:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Partena Disk Cleaning
+
+Because the Citrix hd is full every 2 months.
+
+```ps1
+# Run AsAdmin
+.\partena-disk-clean.ps1
+```
+
+It will:
+- Clean the nuget cache
+- Clean the Recycle bin
+- Clean the ServiceFabric logs
---
+++ partena-disk-clean.ps1
@@ -0,0 +1,25 @@
+# Because the drive is full every 2 months.
+
+# Download nuget: https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+.\nuget.exe locals all -clear
+
+
+
+# Clear-RecycleBin doesn't exist in PowerShell Core?
+Get-ChildItem -Path 'C:\$Recycle.Bin' -Force | Remove-Item -Recurse -ErrorAction SilentlyContinue
+
+
+# Need Administrator permissions for the following:
+
+
+# ServiceFabric:
+# Some code located in: C:\Program Files\Microsoft SDKs\Service Fabric\ClusterSetup
+# But let's do it without installing the ServiceFabric Module:
+Stop-Service FabricHostSvc
+
+Get-ChildItem -Path 'C:\SfDevCluster\Data\ReplicatorLog\*' -Force | Remove-Item
+
+Get-ChildItem -Path 'C:\SfDevCluster\Log\Traces\*' -Force | Remove-Item
+Get-ChildItem -Path 'C:\SfDevCluster\Log\QueryTraces\*' -Force | Remove-Item
+
+Start-Service FabricHostSvc
